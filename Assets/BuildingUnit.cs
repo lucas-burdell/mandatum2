c2d4614eab2c84e3d4a705e31481d8eff7b21120
Lucas Burdell <lucas.burdell@blackburn.edu>
2018-01-30 16:31:59 -0600

    add game

---
+++ Assets/BuildingUnit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mandatum {
+    public class BuildingUnit : Unit {
+        public override int[] GetDamage(Unit other) {
+            throw new System.NotImplementedException();
+        }
+
+        public override void killUnit() {
+            throw new System.NotImplementedException();
+        }
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
